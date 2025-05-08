6a25a47e6d3da6f70be4d29d079600774b077c31
SolomonFoskaay <oskfak@gmail.com>
2025-05-08 15:00:06 +0000

    Updated info for sample dPU dApp Template 2

--- components/navigation/footer.tsx
+++ components/navigation/footer.tsx
@@ -6,7 +6,14 @@ export default function Footer() {
     <footer className="border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          © 2023 Solomon Foskaay. All rights reserved.
+          © 2025 { }
+          <Link 
+          href="https://github.com/dProgrammingUniversity/full-stack-blockchain-dapp-template-for-solana-and-evm"
+          target="_blank"
+          >
+            <u>dPU dApp Template.</u> 
+          </Link>  
+          { }  All rights reserved.
         </p>
         <div className="flex items-center gap-4">
           <Link href="https://github.com/sample" target="_blank" rel="noreferrer">
